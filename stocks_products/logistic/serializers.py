21df12ad4ed2dbac4d969969e31466df776824ba
Aisly <aisly.agdavletova@gmail.com>
2024-10-24 23:21:19 +0300

    Initial commit

---
+++ stocks_products/logistic/serializers.py
@@ -0,0 +1,48 @@
+from rest_framework import serializers
+from logistic.models import Product, Stock, StockProduct
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    # настройте сериализатор для позиции продукта на складе
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity','price']
+
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+    class Meta:
+        model = Stock
+        fields = ['id', 'address', 'positions']
+
+    # настройте сериализатор для склада
+
+    def create(self, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+        # создаем склад по его параметрам
+        stock = super().create(validated_data)
+        for position in positions:
+            StockProduct.objects.create(stock=stock, **position)
+        return stock
+
+    def update(self, instance, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # обновляем склад по его параметрам
+        stock = super().update(instance, validated_data)
+        for position in positions:
+            stock_product = list(StockProduct.objects.filter(stock=stock, product=position['product']))[0]
+            stock_product.stock = stock
+            stock_product.product = position.get('product', stock_product.product)
+            stock_product.quantity = position.get('quantity', stock_product.quantity)
+            stock_product.price = position.get('price',stock_product.price)
+            stock_product.save()
+        return stock
